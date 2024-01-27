34c7e6936c824f22a86dae9cef63056a95360b37
prasanth <prasanth@example.com>
2024-01-27 10:48:12 +0530

    UseReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import { useReducer } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 
 const initialState = {
-  name: "Prasanth",
-  age: 25,
+  search: "",
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
 };
 
 const reducer = (state, action) => {
-  switch(action.type){
-    case 'name':
-      return {...state, name : action.payload.name}
-    case 'age':
-      return {...state, age: action.payload.age}
+  switch (action.type) {
+    case "search":
+      return { ...state, search: action.payload.search };
+    case "Add":
+      return {
+        ...state,
+        items: [...state.items, { id: uuidv4(),item: action.payload.item }],
+        search: "",
+      };
+    default:
+      return state;
   }
-}
+};
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state)
   return (
     <>
-      <input type="text" value={state.name} onChange={(e) => dispatch({type:'name', payload:{name: e.target.value}})}/>
-      <input type="number" value={state.age} onChange={(e) => dispatch({type:'age', payload:{age: e.target.value}})}/>
+      <input
+        type="text"
+        value={state.search}
+        onChange={(e) =>
+          dispatch({ type: "search", payload: { search: e.target.value } })
+        }
+      />
+      <button
+        onClick={() =>
+          dispatch({ type: "Add", payload: { item: state.search } })
+        }
+      >
+        Add
+      </button>
+      {state.items?.map((each, index) => (
+        <div key={index} style={{display:'flex', alignItems:'center'}}>
+          <h5>{each.item}</h5>
+          <button style={{height:'30px'}}>Delete</button>
+        </div>
+      ))}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UseTransition2 from  "./UseTransition2"
-import UseTransition from './UseTransition';
-import Debouunce from './Debouunce';
+// import UseTransition2 from  "./UseTransition2"
+// import UseTransition from './UseTransition';
+// import Debouunce from './Debouunce';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
